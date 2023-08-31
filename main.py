2a2c04dbef7b458f314eb5742dc7d52a2c1c58dd
Aco ylste <79900358+Acoyls@users.noreply.github.com>
2023-08-31 07:25:57 -0300

    menor edad
    xd

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-from flask import Flask, jsonify,render_template
+from flask import Flask, jsonify,render_template,request
 import os
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/',  methods = ('GET','POST'))
 def index():
+    edad = request.form['edad']
+    if edad < 18:
+        return render_template('menoredad.html')
     return render_template('index.html')
 
 
